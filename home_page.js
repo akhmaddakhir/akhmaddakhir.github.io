2cbb3a8f69c2b589aabaeb1c09756b88845e1432
Mulyadisapusapu <kairulkumar50@gmail.com>
2025-11-28 02:50:09 +0700

    uhuyy

--- home_page.js
+++ home_page.js
@@ -1,3 +1,31 @@
+fetch("filter_popup.html")
+  .then((res) => res.text())
+  .then((html) => {
+    document.getElementById("filter-popup").innerHTML = html;
+
+    const openFilterPopup = document.getElementById("filter-button");
+    const closeFilterPopup = document.querySelector(".confirmBtn");
+    const filterPopup = document.getElementById("filterPopup");
+
+    function openPopup() {
+      filterPopup.classList.add("active");
+    }
+
+    function closePopup() {
+     filterPopup.classList.remove("active");
+    }
+
+    openfilterPopup.addEventListener("click", openPopup);
+
+    closefilterPopup.addEventListener("click", closePopup);
+
+    window.addEventListener("click", (event) => {
+      if (event.target === filterPopup) {
+        closePopup();
+      }
+    });
+  });
+
 const saveButtons = document.querySelectorAll(".save-button, .save-button-notag");
 
 saveButtons.forEach((button) => {
